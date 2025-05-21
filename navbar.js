565c04852766fc7515629327fb0a193ee6361a5b
TatyanaQR <tatyanaquinonez@icloud.com>
2025-05-20 20:03:44 -0400

    Delete navbar.js

--- navbar.js
+++
@@ -1,5 +0,0 @@
-
-// JS for dropdown (no extra logic needed currently, but placeholder for future use)
-document.addEventListener("DOMContentLoaded", function () {
-    // Future dropdown interactions can go here
-});
